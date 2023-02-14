3060b2f851fe868e0a98291a01a8010cae3f250a
Butcherok <butcherok231@gmail.com>
2023-02-14 17:26:47 +0200

    add libs + import + maket

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
- 
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+function createImage() {
+    return`
+    <div class="photo-card">
+        <img src="" alt="" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+                <b>Likes</b>
+            </p>
+            <p class="info-item">
+                <b>Views</b>
+            </p>
+            <p class="info-item">
+                <b>Comments</b>
+            </p>
+            <p class="info-item">
+                <b>Downloads</b>
+            </p>
+        </div>
+    </div>
+    `
+}
